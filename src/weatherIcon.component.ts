856386565cac7d64c760a141f13eefe7ff9bb2d4
Damien Suhard <ptitdam2001@gmail.com>
2016-12-14 01:57:28 +0100

    implement version 0.1.3

--- src/weatherIcon.component.ts
+++ src/weatherIcon.component.ts
@@ -1,10 +1,11 @@
 
 import { Component, Input, OnInit } from '@angular/core';
 
-import 'weather-icons/css/weather-icons.css';
-
+import './css/weather-icons.min.css';
+import './css/weather-icons-wind.min.css';
 
 @Component({
+  // moduleId: module.id,
   selector: 'ng2-wi',
   template: `<i [ngClass]="cClass"></i>`,
   styleUrls: [
